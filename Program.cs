439bb278d53a4a7708c44efe9693d4c0565f9083
cemetryghost <96484941+cemetryghost@users.noreply.github.com>
2023-03-04 12:51:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp3
+namespace ConsoleApp3
 {
     internal class Program
     {
@@ -6,13 +6,13 @@
         {
             Student[] students = new Student[7];
 
-            students[0] = new Student("Марусик", "Матвей", "Даниилович", 201, new int[] { 4, 5, 5, 5, 5 });
-            students[1] = new Student("Михотин", "Иван", "Александрович", 211, new int[] { 3, 3, 3, 5, 5 });
-            students[2] = new Student("Иванов", "Дмитрий", "Андреевич", 213, new int[] { 5, 5, 4, 5, 4 });
-            students[3] = new Student("Байкин", "Никита", "Дмитриевич", 201, new int[] { 3, 3, 2, 4, 5});
-            students[4] = new Student("Коновалов", "Денис", "Александрович", 215, new int[] { 4, 4, 4, 4, 3 });
-            students[5] = new Student("Русинов", "Юрий", "Дмитриевич", 291, new int[] { 4, 5, 4, 5, 4 });
-            students[6] = new Student("Иванов", "Никита", "Максимович", 298, new int[] { 4, 3, 5, 3, 5 });
+            students[0] = new ("Марусик", "Матвей", "Даниилович", 201, new int[] { 4, 5, 5, 5, 5 });
+            students[1] = new ("Михотин", "Иван", "Александрович", 211, new int[] { 3, 3, 3, 5, 5 });
+            students[2] = new ("Иванов", "Дмитрий", "Андреевич", 213, new int[] { 5, 5, 4, 5, 4 });
+            students[3] = new ("Байкин", "Никита", "Дмитриевич", 201, new int[] { 3, 3, 2, 4, 5});
+            students[4] = new ("Коновалов", "Денис", "Александрович", 215, new int[] { 4, 4, 4, 4, 3 });
+            students[5] = new ("Русинов", "Юрий", "Дмитриевич", 291, new int[] { 4, 5, 4, 5, 4 });
+            students[6] = new ("Иванов", "Никита", "Максимович", 298, new int[] { 4, 3, 5, 3, 5 });
 
             var sortedStudents = students.OrderBy(n => n.average);
 
